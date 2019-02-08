f49449d1ee6ff3cc9ca8d890d8642ed8b8788152
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-02-09 05:03:41 +0700

    alibrabra

--- back/page/index.php
+++ back/page/index.php
@@ -9,18 +9,29 @@
 
     <!-- bootstrap 4.2.1 -->
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- w3.css -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
 </head>
 
 <body>
-    <!-- body -->
-    <section style="background-color:blue">
-        <div class="container" style="background-color:red">
 
-
-            
-            
+    <div class="row">
+        <div class="container">
+            <div class="w3-sidebar w3-bar-block" style="background-color:red;">
+                <a href="#" class="w3-bar-item w3-button">Link 1</a>
+                <a href="#" class="w3-bar-item w3-button">Link 2</a>
+                <a href="#" class="w3-bar-item w3-button">Link 3</a>
+            </div>
         </div>
-    </section><!-- body -->
+        <div class="col-md-9">
+            <div class="w3-container w3-blue">
+                <h1>I am a Heading</h1>
+                <p>I am a paragraph.</p>
+            </div>
+        </div>
+    </div>
 
     <!-- bootstrap 4.2.1 -->
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
