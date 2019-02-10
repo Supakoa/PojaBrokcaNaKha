2c3c89bdc8fda5bb96b3a1fe4478f33b2a0a498f
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-02-10 18:07:20 +0700

    00.00

--- back/page/index.php
+++ back/page/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Admin Page 1</title>
+    <title>BackDoor : Main</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- bootstrap 4.2.1 -->
@@ -13,30 +13,68 @@
     <!-- w3.css -->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
+    <!-- mycss -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" 
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
 </head>
 
-<body>
+<body class="w3-dark-gray">
 
-    <div class="row">
-        <div class="container">
-            <div class="w3-sidebar w3-bar-block" style="background-color:red;">
-                <a href="#" class="w3-bar-item w3-button">Link 1</a>
-                <a href="#" class="w3-bar-item w3-button">Link 2</a>
-                <a href="#" class="w3-bar-item w3-button">Link 3</a>
-            </div>
-        </div>
-        <div class="col-md-9">
-            <div class="w3-container w3-blue">
-                <h1>I am a Heading</h1>
-                <p>I am a paragraph.</p>
-            </div>
+    <!-- start sidebar -->
+    <div class="w3-sidebar w3-bar-block w3-red" style="width:15%;border:solid;border-color:red;">
+        <h1 class="w3-center"><i class="fas fa-home"></i> Home</h1>
+        <div class="w3-container" style="margin-left:1%;">
+            <a href="#" class="w3-bar-item w3-button"><i class="far fa-folder-open"></i> Document</a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fas fa-user-edit"></i> Member</a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fas fa-bullhorn"></i> Advertise</a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fas fa-sort-amount-down"></i> Sender</a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fas fa-inbox"></i> Inbox<span class="w3-badge w3-right w3-white">7</span></a>
         </div>
     </div>
+    <!-- end sidebar -->
 
-    <!-- bootstrap 4.2.1 -->
-    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="../node_modules/popper.js/dist/popper.min.js"></script>
-    <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <div style="margin-left:15%;">
+
+        <!-- start NavBar -->
+        <div class="w3-bar w3-green" style="border:solid;border-color:green;">
+            <a class="w3-bar-item w3-blue w3-text w3-center" style="border:solid;border-color:blue;width:79%;">Page : Home</a>
+            <!-- set to right -->
+            <div class="w3-right">
+                <a href="#" class="w3-bar-item w3-button w3-padding-large"><i class="far fa-user-circle"></i> Profile</a>
+                <a href="#" class="w3-bar-item w3-button w3-padding-large"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+        </div>
+        <!-- End NavBar -->
+
+        <!-- start body -->
+        <div class="w3-container w3-card-4 w3-purple" style="border:solid;border-color:purple;height:530px" id="body">
+            <h1>Body</h1>
+        </div>
+        <!-- end body -->
+
+        <!-- set to bottom -->
+        <div class="w3-container-fluid " style="text-align:center;">
+            <!-- prefooter -->
+            <div class="w3-orange" style=";border:solid;border-color:orange;" id="prefooter">
+                <h1>Footer</h1>
+            </div>
+            <!-- footer -->
+            <div class="w3-black" style="border:solid;border-color:black;" id="footer">
+                <p style="margin-top:1%">©Powered by CEFStyle</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+<!-- bootstrap 4.2.1 -->
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="../node_modules/popper.js/dist/popper.min.js"></script>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
