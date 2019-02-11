080d4e2e6c622ec43e5369d481d2cdf3da78bf2b
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-02-11 23:42:07 +0700

    koko

--- back/page/member.php
+++ back/page/member.php
@@ -1,1 +1,4 @@
-2
+<div class="w3-container-fluid w3-center w3-red" style="margin:20px;height:477px;">
+    <button type="button" class="btn btn-outline-dark" style="margin:15px;height:450px;width:450px;font-size:50px;"><strong>All</strong></button>
+    <button type="button" class="btn btn-outline-dark" style="margin:15px;height:450px;width:450px;font-size:50px;"><strong>Specific</strong></button>
+</div>
