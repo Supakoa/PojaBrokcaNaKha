a11b1cccb6350406d29ec5a5fc009c1c40d80496
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-02-12 10:22:15 +0700

    change main button

--- back/page/main.php
+++ back/page/main.php
@@ -2,21 +2,21 @@
     <h1 style="margin:30px">MENU</h1><hr>
     <div class="w3-row w3-yellow w3-margin" style="border:solid;border-color:yellow;">
 
-        <div onclick="call_content('document.php','Page : Document','BackDoor : Document');" class="w3-col w3-btn w3-gray"
-            style="height:200px;width:200px;margin:15px;">Document</div>
+        <button onclick="call_content('document.php','Page : Document','BackDoor : Document');" class="w3-col w3-btn w3-gray"
+            style="height:200px;width:200px;margin:15px;">Document</button>
 
-        <div onclick="call_content('member.php','Page : Member','BackDoor : Member');" class="w3-col w3-btn w3-gray"
-            style="height:200px;width:200px;margin:15px;">Member</div>
+        <button onclick="call_content('member.php','Page : Member','BackDoor : Member');" class="w3-col w3-btn w3-gray"
+            style="height:200px;width:200px;margin:15px;">Member</button>
 
 
-        <div onclick="call_content('news.php','Page : Advertise','BackDoor : Advertise');" class="w3-col w3-btn w3-gray"
-            style="height:200px;width:200px;margin:15px;">Advertise</div>
+        <button onclick="call_content('news.php','Page : Advertise','BackDoor : Advertise');" class="w3-col w3-btn w3-gray"
+            style="height:200px;width:200px;margin:15px;">Advertise</button>
 
-        <div onclick="call_content('sender.php','Page : Sender','BackDoor : Sender');" class="w3-col w3-btn w3-gray"
-            style="height:200px;width:200px;margin:15px;">Sender</div>
+        <button onclick="call_content('sender.php','Page : Sender','BackDoor : Sender');" class="w3-col w3-btn w3-gray"
+            style="height:200px;width:200px;margin:15px;">Sender</button>
 
-        <div onclick="call_content('Inbox.php','Page : Inbox','BackDoor : Inbox');" class="w3-col w3-btn w3-gray" 
-            style="height:200px;width:200px;margin:15px;">Inbox</div>
+        <button onclick="call_content('Inbox.php','Page : Inbox','BackDoor : Inbox');" class="w3-col w3-btn w3-gray" 
+            style="height:200px;width:200px;margin:15px;">Inbox</button>
             
     </div>
 </div>
