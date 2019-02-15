c3f41f6b25deab7cb81a4c7de2ca6056e59105c9
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-02-15 23:57:56 +0700

    5

--- test.php
+++ test.php
@@ -1,11 +1,21 @@
-<?php
-$to = "singcomnet@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-if(mail($to,$subject,$txt,$headers)){
-    echo "gogo";
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+<script src="push.js"></script>
+<script src="font/node_modules/jquery/dist/jquery.min.js"></script>
+</head>
+<body>
+    
+    <button id = "eiei">กดหน่อยจ่ะ</button>
+</body>
+<script>
+ $("#eiei").click(function(){
+    Push.create('Hello World!')
+ });
+</script>
+</html>
