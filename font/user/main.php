c66de3d2b8f89c9b7a8f7f5bb9f8b2096dc82edb
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-02-19 02:34:46 +0700

    หก

--- font/user/main.php
+++ font/user/main.php
@@ -123,8 +123,20 @@ $sub = '<label for="sub">วิชา</label>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
+                                                       <tr>
+                                                            <td>1</td>
+                                                            <td><span class="badge badge-info">กำลังดำเนินการ</span></td>
+                                                            <td>ลากิจ/ลาป่วย</td>
+                                                            <td>
+                                                                <button type="button" class="btn btn-info btn-sm"
+                                                                    data-toggle="modal" data-target="#route">
+                                                                    แสดง
+                                                                </button>
+                                                            </td>
+                                                            <td> - </td>
+                                                        </tr>
                                                         <tr>
-                                                            <th scope="row">1</th>
+                                                            <th scope="row">2</th>
                                                             <td><span class="badge badge-danger">ไม่ผ่าน</span></td>
                                                             <td>ขอสอบย้อนหลัง</td>
                                                             <td>
@@ -186,6 +198,7 @@ $sub = '<label for="sub">วิชา</label>
                                                                 <p>เอกสารไม่ครบ</p>
                                                             </td>
                                                         </tr>
+                                                        
                                                     </tbody>
                                                 </table>
                                             </div>
@@ -511,13 +524,18 @@ $sub = '<label for="sub">วิชา</label>
                                                         <thead>
                                                             <tr>
                                                                 <th>status</th>
+                                                                <th>เวลาส่ง</th>
+                                                                <th>เรื่อง</th>
                                                                 <th>ข้อความ</th>
                                                                 <th>ผู้ส่ง</th>
+
                                                             </tr>
                                                         </thead>
                                                         <tbody>
                                                             <tr>
                                                                 <td><span class="badge badge-danger">ยังไม่ได้อ่าน</span></td>
+                                                                <td>2019-01-18 14:20:47</td>
+                                                                <td>เลือกแบบคำร้องไม่ถูกครับ</td>
                                                                 <td>
                                                                     <!-- Small modal -->
                                                                     <button type="button" class="btn btn-info btn-sm"
@@ -546,6 +564,8 @@ $sub = '<label for="sub">วิชา</label>
                                                             </tr>
                                                             <tr>
                                                                 <td><span class="badge badge-success">อ่านแล้ว</span></td>
+                                                                <td>2019-01-17 18:33:24</td>
+                                                                <td>แนบเอกสารลากิจ</td>
                                                                 <td>
                                                                     <!-- Small modal -->
                                                                     <button type="button" class="btn btn-info btn-sm"
