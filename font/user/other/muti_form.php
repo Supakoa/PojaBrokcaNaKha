ac85006dff0402ca5d0fb96debd454b06e026a78
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-03-10 22:54:39 +0700

    asd

--- font/user/other/muti_form.php
+++ font/user/other/muti_form.php
@@ -1,15 +1,33 @@
 <?php 
 require '../../../server/server.php';
 $id = $_SESSION['id'];
+function getToken($length){
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet .= "0123456789";
+    $max = strlen($codeAlphabet); // edited
+   for ($i=0; $i < $length; $i++) {
+       $token .= $codeAlphabet[random_int(0, $max-1)];
+   }
+   return $token;
+}
+$paper_id = getToken(10);
 if(isset($_POST['form_1'])){
+
    $detail= $_POST['sub']."๛".$_POST['group'];
-    $sql_paper = "INSERT INTO `paper`( `owner_id`, `form_id`, `paper_detail`, `step_now`, `status`) VALUES ('$id','1','$detail','1','0') ";
+    $sql_paper = "INSERT INTO `paper`( `paper_id` `owner_id`, `form_id`, `paper_detail`, `step_now`, `status`) VALUES ('$paper_id', '$id','1','$detail','1','3') ";
     if($re_paper = mysqli_query($con,$sql_paper)){
                 $_SESSION['alert'] = 3;
     }else{
         $_SESSION['alert'] = 4;
     }
-   echo $detail;
+    $sql_form = "SELECT * FROM `form_way` WHERE `form_id` = '1' AND `step` ='1' ";
+    $re_form = mysqli_query($con, $sql_form);
+    while($row_form = mysqli_fetch_array($re_form)){
+        $sql_user = " INSERT INTO `paper_user`( 'paper_id', `user_id`,) VALUES ('$paper_id','$row_form['user_id']' )";
+        $re_user = mysqli_query($con, $sql_user);
+        $row_user = mysqli_fetch_array($re_user);
+    }
  
 }elseif(isset($_POST['form_2'])){
     echo "2";
