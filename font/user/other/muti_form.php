3cb1fa3184d358a7d4716d9075893886bc466160
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-03-11 00:21:33 +0700

    koa

--- font/user/other/muti_form.php
+++ font/user/other/muti_form.php
@@ -50,11 +50,52 @@ if(isset($_POST['form_1'])){
     echo "5";
 
 }elseif(isset($_POST['form_6'])){
-    echo "6";
+    $detail= $_POST['sub']."๛".$_POST['group']."๛".$_POST['type']."๛".$_POST['note'];
+    $sql_paper = "INSERT INTO `paper`( `paper_id`,`owner_id`, `form_id`, `paper_detail`, `step_now`, `status`) VALUES ('$paper_id', '$id','6','$detail','1','3') ";
+    if($re_paper = mysqli_query($con,$sql_paper)){
+                $_SESSION['alert'] = 3;
+                $sql_form = "SELECT * FROM `form_way` WHERE `form_id` = '1' AND `step` ='1' ";
+                $re_form = mysqli_query($con, $sql_form);
+                $row_form = mysqli_fetch_array($re_form);
+                $user_id = $row_form['user_id'];
+                $sum_q = '(\''.$paper_id.'\',\''.$user_id.'\' )';
+                while($row_form = mysqli_fetch_array($re_form)){
+                    $user_id = $row_form['user_id'];
+                    $sum_q .= ',(\''.$paper_id.'\',\''.$user_id.'\' )';
+                }
+                $sql_user = "INSERT INTO `paper_user`( `paper_id`, `user_id`) VALUES ".$sum_q." ";
+                if($re_user = mysqli_query($con, $sql_user)){
+                    $_SESSION['alert'] = 3;
+                }else{
+                    $_SESSION['alert'] = 4;
+                }
+    }else{
+        $_SESSION['alert'] = 4;
+    }
 
 }elseif(isset($_POST['form_7'])){
-    echo "7";
-
+    $detail= $_POST['comment']."๛".$_POST['group'];
+    $sql_paper = "INSERT INTO `paper`( `paper_id`,`owner_id`, `form_id`, `paper_detail`, `step_now`, `status`) VALUES ('$paper_id', '$id','7','$detail','1','3') ";
+    if($re_paper = mysqli_query($con,$sql_paper)){
+                $_SESSION['alert'] = 3;
+                $sql_form = "SELECT * FROM `form_way` WHERE `form_id` = '1' AND `step` ='1' ";
+                $re_form = mysqli_query($con, $sql_form);
+                $row_form = mysqli_fetch_array($re_form);
+                $user_id = $row_form['user_id'];
+                $sum_q = '(\''.$paper_id.'\',\''.$user_id.'\' )';
+                while($row_form = mysqli_fetch_array($re_form)){
+                    $user_id = $row_form['user_id'];
+                    $sum_q .= ',(\''.$paper_id.'\',\''.$user_id.'\' )';
+                }
+                $sql_user = "INSERT INTO `paper_user`( `paper_id`, `user_id`) VALUES ".$sum_q." ";
+                if($re_user = mysqli_query($con, $sql_user)){
+                    $_SESSION['alert'] = 3;
+                }else{
+                    $_SESSION['alert'] = 4;
+                }
+    }else{
+        $_SESSION['alert'] = 4;
+    }
 }
 else{
     echo "8";
