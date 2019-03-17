723c378e9ce80dea1f1dda810177e61d730559ee
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-03-17 19:50:38 +0700

    asddd

--- back/send_sql/add_group.php
+++ back/send_sql/add_group.php
@@ -2,6 +2,24 @@
     // connect database
     require '../../server/server.php';
       //POST
-      $id = $_POST['id_del'];
+      $name = $_POST['name'];
+      $type = $_POST['type'];
+      
+      $sql = "INSERT INTO `groups`(`name`, `type`) VALUES ([value-2],[value-3]) ";
+     if( $re_add = mysqli_query($con,$sql)){
+        $alert = 3;
+     }
+     else{
+        $alert = 4;
+     }
+    //  print_r($_POST);
+    //   echo($name.' : '.$type);
 
 ?>
+<input type="hidden" id="alert" value = "">
+
+<script>
+a = <?php echo $alert; ?> ;
+    $("#alert").val(a);
+   
+</script>
