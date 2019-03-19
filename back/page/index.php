43aa74c350415430f7db5576b68435b9afa6cf14
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-03-19 21:22:14 +0700

    minecraft at a alert

--- back/page/index.php
+++ back/page/index.php
@@ -28,7 +28,8 @@ require '../../server/server.php';
     <link rel="stylesheet" href="style.css">
 
     <!-- fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+        crossorigin="anonymous">
 
 
 
@@ -56,9 +57,15 @@ require '../../server/server.php';
         <a href="#" onclick="call_content('sender.php','หน้า : ขั้นตอนเอกสาร','BackDoor : ขั้นตอนเอกสาร');">
             <i class="fa fa-sort-amount-down"></i> ขั้นตอนเอกสาร
         </a>
-        <a href="#" onclick="call_content('groups.php','หน้า : ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม','BackDoor : ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม');">
-            <i class="fa fa-sort-amount-down"></i> ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม
-        </a>
+        <a href="#" onclick="call_content('groups.php','หน้า : ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม','BackDoor : ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม');"><i
+                class="far fa-edit"></i>
+            ตั้งค่ากลุ่มผู้มีสิทธิ์ลงนาม</a>
+        <a href="#" onclick="call_content('subject.php','หน้า : ตั้งค่ารายชื่อวิชา','BackDoor : ตั้งค่ารายชื่อวิชา');"><i
+                class="fas fa-book"></i>
+            ตั้งค่ารายชื่อวิชา</a>
+        <a href="#" onclick="call_content('faculty.php','หน้า : ตั้งค่ารายชื่อคณะ/สาขา','BackDoor : ตั้งค่ารายชื่อคณะ/สาขา');"><i
+                class="fas fa-user-graduate"></i>
+            ตั้งค่ารายชื่อคณะ/สาขา</a><hr>
     </div>
     <!-- rigth nav -->
     <div id="main">
@@ -70,16 +77,19 @@ require '../../server/server.php';
 
             <a id="nav_title" href="#" class="navbar-brand"></a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a href="#" class="nav-link" style="color:whitesmoke;"><i class="far fa-user-circle" style="color:whitesmoke;"></i> ข้อมูลส่วนตัว</a>
+                        <a href="#" class="nav-link" style="color:whitesmoke;"><i class="far fa-user-circle" style="color:whitesmoke;"></i>
+                            ข้อมูลส่วนตัว</a>
                     </li>
                     <li class="nav-item ">
-                        <a href="#" class="nav-link" style="color:whitesmoke;"><i class="fas fa-sign-out-alt" style="color:whitesmoke;"></i> ออกจากระบบ</a>
+                        <a href="#" class="nav-link" style="color:whitesmoke;"><i class="fas fa-sign-out-alt" style="color:whitesmoke;"></i>
+                            ออกจากระบบ</a>
                     </li>
                 </ul>
             </div>
@@ -123,10 +133,10 @@ require '../../server/server.php';
     //             });
 </script>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         // $('.table_table').DataTable();
 
-        $("#btn_singha").click(function() {
+        $("#btn_singha").click(function () {
 
             $('#btn_singha').text("กดแล้ว");
 
@@ -176,4 +186,4 @@ require '../../server/server.php';
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
 
-</html> 
+</html>
