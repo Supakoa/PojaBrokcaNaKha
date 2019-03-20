7fb2d40a778a04c2368b7c15168c0d5c89626616
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-03-21 01:47:17 +0700

    Update student

--- back/page/subject.php
+++ back/page/subject.php
@@ -3,7 +3,7 @@
     require '../../server/server.php';
 ?>
 
-<div class="container text-center" style="margin:20px;">
+<div class="container mx-auto text-center" style="margin:20px;align:center;">
     <div class="container-fluid " style="margin:20px;padding:20px;">
 
         <div id="head">
@@ -19,6 +19,7 @@
                         <th>ลำดับ</th>
                         <th>รหัสวิชา</th>
                         <th>ชื่อวิชา</th>
+                        <th>แก้ไข</th>
                     </tr>
                 </thead><!-- table head -->
 
@@ -28,27 +29,36 @@
                         $result = mysqli_query($con,$sql_sub_all);
                         $i = 0;
                         while($row_sub_all = mysqli_fetch_array($result)){ ?>
-                    <tr onclick="">
+                    <tr>
                         <td>
-                            <?php echo (++$i).').' ; ?>
+                            <?php echo (++$i).').' ;$id = $row_sub_all[0];$name = $row_sub_all[1]; ?>
+                            <div class="collapse" id="containt_<?php echo $i; ?>">
+                                <hr>
+                                <div id="update_<?php echo $i; ?>" onclick="update_sub(<?php echo $i; ?>);" class="btn btn-sm btn-success"><i class="far fa-check-circle" style="color:white;"></i></div>
+                            </div><!-- collapse -->
                         </td>
                         <td>
                             <?php echo $row_sub_all['sub_id']; ?>
+                            <div class="collapse" id="containt_<?php echo $i; ?>">
+                                <hr><input id="input_id_<?php echo $i; ?>" onchange="inputChange(<?php echo $i; ?>)" class="form-control form-control-sm" type="text" style="text-align:center;"
+                                    placeholder="<?php echo $row_sub_all['sub_id']; ?>">
+                            </div><!-- collapse -->
                         </td>
                         <td>
                             <?php echo $row_sub_all['sub_name']; ?>
+
+                            <div class="collapse" id="containt_<?php echo $i; ?>">
+                                <hr><input id="input_name_<?php echo $i; ?>" onchange="inputChange(<?php echo $i; ?>)" class="form-control form-control-sm" type="text" style="text-align:center;"
+                                    placeholder="<?php echo $row_sub_all['sub_name']; ?>">
+                            </div><!-- collapse -->
                         </td>
-                    </tr>
-                    <tr style="display:;">
-                        <td></td>
-                        <td><input class="form-control form-control-sm" type="text" style="text-align:center;"
-                                placeholder="<?php echo $row_sub_all['sub_id']; ?>"></td>
                         <td>
-                            <input class="form-control form-control-sm" type="text" style="text-align:center;"
-                                placeholder="<?php echo $row_sub_all['sub_name']; ?>">
-                            <div class="btn btn-sm btn-success">update</div>
+                            <button type="button" onclick="changeIcon(<?php echo $i; ?>);" class="btn btn-sm btn-outline-dark" data-toggle="collapse" aria-expanded="false"
+                                aria-controls="collapseExample" data-target="#containt_<?php echo $i; ?>"><i id="icon_<?php echo $i; ?>" class="fas fa-angle-double-down"></i></button>
                         </td>
+
                     </tr>
+
                     <?php } ?>
                 </tbody>
             </table>
@@ -56,3 +66,35 @@
 
     </div>
 </div>
+
+<script>
+    function changeIcon(i){
+        if($("#icon_"+i).attr('class') == 'fas fa-angle-double-down'){
+            $("#icon_"+i).removeClass("fas fa-angle-double-down");
+            $("#icon_"+i).addClass("fas fa-angle-double-up");
+        }
+        else if($("#icon_"+i).attr('class') == 'fas fa-angle-double-up'){
+            $("#icon_"+i).removeClass("fas fa-angle-double-up");
+            $("#icon_"+i).addClass("fas fa-angle-double-down");
+        }
+    } // change icon
+
+    function inputChange(i){
+        // alert($("#input_name_"+i).val()+" : "+$("#input_id_"+i).val());
+        if($("#input_name_"+i).val() === "" && $("#input_id_"+i).val() === ""){
+            // alert('a');
+            $("#update_"+i).removeClass("btn btn-sm btn-warning");
+            $("#update_"+i).addClass("btn btn-sm btn-success");
+        }else if($("#input_name_"+i).val() !== "" || $("#input_id_"+i).val() !== ""){
+            // alert('b');
+            $("#update_"+i).removeClass("btn btn-sm btn-success");
+            $("#update_"+i).addClass("btn btn-sm btn-warning");
+        }
+    }
+
+
+    function update_sub(i){
+        
+    }
+
+</script>
