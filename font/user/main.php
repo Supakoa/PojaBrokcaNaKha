fbe2a48c14e9f19f44160e76d5878076f3d3c3a9
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-03-31 21:23:16 +0700

    koa

--- font/user/main.php
+++ font/user/main.php
@@ -536,7 +536,7 @@ if (isset($_POST['senmessage'])) {
                                         <div class="card">
                                             <!-- card 2.1 -->
                                             <div class="card-body">
-                                                <div class="table-responsive-lg ">
+                                                <div class="table-responsive">
                                                     <table id="table2" class="table table-striped table-hover display nowrap responsive">
                                                         <thead>
                                                             <tr>
