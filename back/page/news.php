e3bd3f3ba68d5acf3121d01f815d0e0ffdfba5cf
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-03-31 22:33:36 +0700

    koa

--- back/page/news.php
+++ back/page/news.php
@@ -1,11 +1,10 @@
 <?php
-    require '../../server/server.php';
-    $sql = "SELECT * FROM news WHERE 1";
-    $re_news = mysqli_query($con,$sql);
-    $order = 0;
+require '../../server/server.php';
+$sql = "SELECT * FROM news WHERE 1";
+$re_news = mysqli_query($con, $sql);
+$order = 0;
 ?>
 <div class="container-fluid ">
-    <div class="container" >
         <h3>ตั้งค่า : ข่าว</h3>
         <hr>
 
@@ -15,53 +14,35 @@
                 เพิ่ม</button>
         </div><br>
 
-        <div class="container-fluid" >
-            <!-- start add -->
-            <div class="row" style="border:solid;background-color: #C5EDD8;">
-                <div class="col-1 ">
-                    <p>ลำดับ</p>
-                </div>
-                <div class="col">
-                    <p>ที่อยู่เว็บไซต์</p> 
-                </div>
-                <div class="col">
-                     <p>หน้าปก</p> 
-                </div>
-                <div class="col-2">
-                    ลบ
-                </div>
-            </div>
-            <!-- end add -->
+        <div class="container-fluid table-responsive text-center ">
+            <table class="table table-borderless responsive nowrap table-hover table_table">
+                <thead>
+                    <tr>
+                        <th>ลำดับ</th>
+                        <th>ที่อยู่เว็บไซต์</th>
+                        <th>หน้าปก</th>
+                        <th>ลบ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row_news = mysqli_fetch_array($re_news)) {
+                        ?>
+                    <tr>
+                        <td><?php echo ++$order . ")"; ?></td>
+                        <td> <a href="#" target="_blank"><?php echo $row_news['news_url']; ?></a></td>
+                        <td> <img class="w3-red" src="..\image\news\<?php echo $row_news['news_img']; ?>" width="200" height="100"></td>
+                        <td >
+                            <button class="btn btn-lg btn-white" onclick="openDelModal(<?php echo $row_news['news_id'] ?>,'<?php echo $order; ?>');"><i class="fas fa-trash"></i></button>
+                        </td>
+                    </tr>
+                    <?php 
+                } ?>
 
-            <!-- start NEWS template -->
-            <?php
-                while($row_news = mysqli_fetch_array($re_news)){
-            ?>
-            <div class="row" style="padding:20px;border:solid;">
-                <div class="col-1">
-                    <p><?php echo ++$order; ?>).</p>
-                </div>
-                <div class="col">
-                    <a href="#" target="_blank"><?php echo $row_news['news_url']; ?></a>
-                </div>
-                <div class="col">
-                    <img class="w3-red" src="..\image\news\<?php echo $row_news['news_img'];?>" width="200" height="100">
-                </div>
-                <div class="col-2">
-                    <button class = "btn btn-lg btn-info" onclick="openDelModal(<?php echo $row_news['news_id'] ?>,'<?php echo $order; ?>');"><i class="fas fa-trash" style = "color:red"></i></button>
-                </div>
-            </div>
-                <?php } ?>
-            <!-- end NEWS template -->
+                </tbody>
+            </table>
 
         </div>
-        <!-- <div class="w3-container-fluid w3-center" style="margin:20px;">
-            <button type="button" class="btn btn-primary btn-lg"><i class="fas fa-sync-alt"></i> Update</button>
-            <button class="btn btn-primary btn-lg" type="button" disabled>
-                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                Loading...
-            </button> 
-        </div> -->
 
 
         <!-- start modal delete -->
@@ -92,25 +73,30 @@
         <!-- modal delete ajax -->
         <div class="" id="news_mo"></div>
 
-    </div>
 </div>
 
 <script>
-    function openDelModal(id_del,order){
-        $.post("../modal/news_del.php", 
-            { del_id:id_del , order:order },
-            function (data, textStatus, jqXHR) {
+    $(document).ready(function () {
+        $('.table_table').DataTable();
+    });
+    function openDelModal(id_del, order) {
+        $.post("../modal/news_del.php", {
+                del_id: id_del,
+                order: order
+            },
+            function(data, textStatus, jqXHR) {
                 $("#news_mo").html(data);
                 $("#new_del").modal("show");
             }
         )
     }
+
     function openAddModal() {
         $.post("../modal/news_add.php",
-            function (data, textStatus, jqXHR) {
+            function(data, textStatus, jqXHR) {
                 $("#news_mo").html(data);
                 $("#add_modal").modal("show");
             }
         )
     }
-</script>
+</script> 
--- back/page/subject.php
+++ back/page/subject.php
@@ -1,78 +1,64 @@
 <?php
     // connect database server
-    require '../../server/server.php';
+require '../../server/server.php';
 ?>
 
-<div class="container mx-auto" >
-    <div class="container-fluid " >
+<div class="container-fluid mx-auto">
+    <div id="head">
+        <h3>ตั้งค่า : รายชื่อวิชา</h3>
+        <hr>
+    </div><!-- head -->
 
-        <div id="head">
-            <h3>ตั้งค่า : รายชื่อวิชา</h3>
-            <hr>
-        </div><!-- head -->
+    <div  class="table-responsive">
+        <table class="table table-bordered table-hover display nowrap responsive table_table">
 
-        <div id="body" class="table-responsive-lg" >
-            <table class="table table-bordered table-sm table-hover display text-center nowrap responsive">
+            <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                    <th>รหัสวิชา</th>
+                    <th>ชื่อวิชา</th>
+                    <th>ลบ</th>
+                </tr>
+            </thead><!-- table head -->
 
-                <thead>
-                    <tr>
-                        <th>ลำดับ</th>
-                        <th>รหัสวิชา</th>
-                        <th>ชื่อวิชา</th>
-                        <th>ลบ</th>
-                    </tr>
-                </thead><!-- table head -->
+            <tbody>
+                <?php
+                $sql_sub_all = "SELECT * FROM `subject` WHERE 1";
+                $result = mysqli_query($con, $sql_sub_all);
+                $i = 0;
+                while ($row_sub_all = mysqli_fetch_array($result)) { ?>
+                <tr>
+                    <td>
+                        <?php echo (++$i) . ').';
+                        $id = $row_sub_all[0];
+                        $name = $row_sub_all[1]; ?>
+                    </td>
+                    <td>
+                        <?php echo $row_sub_all['sub_id']; ?>
+                    </td>
+                    <td>
+                        <?php echo $row_sub_all['sub_name']; ?>
+                    </td>
+                    <td class="text-center">
+                        <button id="<?php echo $row_sub_all['sub_id']; ?>" class="btn btn-sm btn-outline-dark" onclick="delete_sub(this);"><i class="far fa-trash-alt"></i></button>
+                    </td>
 
-                <tbody>
-                    <?php
-                        $sql_sub_all = "SELECT * FROM `subject` WHERE 1";
-                        $result = mysqli_query($con,$sql_sub_all);
-                        $i = 0;
-                        while($row_sub_all = mysqli_fetch_array($result)){ ?>
-                    <tr>
-                        <td>
-                            <?php echo (++$i).').' ;$id = $row_sub_all[0];$name = $row_sub_all[1]; ?>
-                            <!-- <div class="collapse" id="containt_<?php //echo $i; ?>">
-                                <hr>
-                                <div id="update_<?php //echo $i; ?>" onclick="update_sub(<?php //echo $i; ?>);" class="btn btn-sm btn-success"><i class="far fa-check-circle" style="color:white;"></i></div>
-                            </div> --><!-- collapse -->
-                        </td>
-                        <td>
-                            <?php echo $row_sub_all['sub_id']; ?>
-                            <!-- <div class="collapse" id="containt_<?php //echo $i; ?>">
-                                <hr><input id="input_id_<?php //echo $i; ?>" onchange="inputChange(<?php //echo $i; ?>)" class="form-control form-control-sm" type="text" style="text-align:center;"
-                                    placeholder="<?php //echo $row_sub_all['sub_id']; ?>">
-                            </div> --><!-- collapse -->
-                        </td>
-                        <td>
-                            <?php echo $row_sub_all['sub_name']; ?>
+                </tr>
 
-                            <!-- <div class="collapse" id="containt_<?php //echo $i; ?>">
-                                <hr><input id="input_name_<?php //echo $i; ?>" onchange="inputChange(<?php //echo $i; ?>)" class="form-control form-control-sm" type="text" style="text-align:center;"
-                                    placeholder="<?php //echo $row_sub_all['sub_name']; ?>">
-                            </div> --><!-- collapse -->
-                        </td>
-                        <td>
-                            <!-- <button type="button" onclick="changeIcon(<?php //echo $i; ?>);" class="btn btn-sm btn-outline-dark" data-toggle="collapse" aria-expanded="false"
-                                aria-controls="collapseExample" data-target="#containt_<?php //echo $i; ?>"><i id="icon_<?php //echo $i; ?>" class="fas fa-angle-double-down"></i></button> -->
+                <?php 
+            } ?>
+            </tbody>
+        </table><br>
+        <button class="btn btn-block btn-success shadow" onclick="insert_sub();"><i class="fas fa-plus" style="color:white;"></i></button>
+    </div><!-- body -->
 
-                            <button id="<?php echo $row_sub_all['sub_id']; ?>" class="btn btn-sm btn-outline-dark" onclick="delete_sub(this);" ><i class="far fa-trash-alt"></i></button>
-                        </td>
-
-                    </tr>
-
-                    <?php } ?>
-                </tbody>
-            </table>
-            <button class="btn btn-block btn-success shadow" onclick="insert_sub();"><i class="fas fa-plus" style="color:white;"></i></button>
-        </div><!-- body -->
-
-    </div>
 </div>
 
 <script>
-
-    function delete_sub(elem){
+$(document).ready(function () {
+    $('.table_table').DataTable();
+});
+    function delete_sub(elem) {
 
         // get element id name to read and send to ajax
         let id = $(elem).attr('id');
@@ -87,29 +73,31 @@
             confirmButtonText: 'ลบ',
             cancelButtonText: 'ยกเลิก'
         }).then((result) => {
-            if(result.dismiss === Swal.DismissReason.confirm){
-               //calling ajax 
+            if (result.dismiss === Swal.DismissReason.confirm) {
+                //calling ajax 
                 $.ajax({
                     type: "POST",
                     url: "../send_sql/subject_del_sql.php",
-                    data: {id:id},
-                    success: function (data) {
+                    data: {
+                        id: id
+                    },
+                    success: function(data) {
                         // return result of sql
-                        if(data == 'true'){
+                        if (data == 'true') {
                             Swal.fire({
                                 toast: true,
                                 position: 'top-end',
-                                showConfirmButton:false,
-                                timer:3000,
+                                showConfirmButton: false,
+                                timer: 3000,
                                 type: 'success',
                                 titleText: 'ลบสำเร็จ',
                             });
-                        }else{
+                        } else {
                             Swal.fire({
                                 toast: true,
                                 position: 'top-end',
-                                showConfirmButton:false,
-                                timer:3000,
+                                showConfirmButton: false,
+                                timer: 3000,
                                 type: 'warning',
                                 titleText: 'เกิดข้อผลิดพลาด',
                             });
@@ -118,44 +106,42 @@
                 }).then((result) => {
                     // refresh page in index.php by jquery
                     $("#in_body").load("../page/subject.php");
-                }) 
+                })
             }
         })
 
     }
 
     // function add new subject
-    function insert_sub(){
-        let id,name;
+    function insert_sub() {
+        let id, name;
         Swal.mixin({
             input: 'text',
             confirmButtonText: 'Next &rarr;',
             progressSteps: ['1', '2']
-        }).queue([
-            {
+        }).queue([{
                 title: 'รหัสวิชา',
                 text: 'Subject ID.'
             },
             {
-                title:'ชื่อวิชา',
-                text:'Subject Name.'
+                title: 'ชื่อวิชา',
+                text: 'Subject Name.'
             }
         ]).then((result) => {
-            if( result.value[0] == "" || result.value[1] == "" ){
+            if (result.value[0] == "" || result.value[1] == "") {
                 Swal.fire({
                     type: 'error',
-                    title:'ผิดพลาด',
+                    title: 'ผิดพลาด',
                     text: 'กรุณากรอกข้อมูลให้ครบตามที่กำหนด'
                 });
-            }else{
+            } else {
                 id = result.value[0];
                 name = result.value[1];
                 Swal.fire({
                     title: 'ตรวจสอบก่อนส่ง',
-                    html:
-                        'Your subject answers: <br><br><p>' +
-                            '(รหัสวิชา/ID): ' + result.value[0] + '<br>' +
-                            '(ชื่อวิชา/name): ' + result.value[1] + '<br>' +
+                    html: 'Your subject answers: <br><br><p>' +
+                        '(รหัสวิชา/ID): ' + result.value[0] + '<br>' +
+                        '(ชื่อวิชา/name): ' + result.value[1] + '<br>' +
                         '</p>',
                     confirmButtonText: 'ยืนยัน'
                 }).then(() => {
@@ -164,23 +150,26 @@
                     $.ajax({
                         type: "POST",
                         url: "../send_sql/subject_add_sql.php",
-                        data: {id:id,name:name},
-                        success: function (data) {
-                            if(data == 'true'){
+                        data: {
+                            id: id,
+                            name: name
+                        },
+                        success: function(data) {
+                            if (data == 'true') {
                                 Swal.fire({
                                     toast: true,
                                     position: 'top-end',
-                                    showConfirmButton:false,
-                                    timer:3000,
+                                    showConfirmButton: false,
+                                    timer: 3000,
                                     type: 'success',
                                     titleText: 'เพิ่มข้อมูลสำเร็จ',
                                 });
-                            }else{
+                            } else {
                                 Swal.fire({
                                     toast: true,
                                     position: 'top-end',
-                                    showConfirmButton:false,
-                                    timer:3000,
+                                    showConfirmButton: false,
+                                    timer: 3000,
                                     type: 'warning',
                                     titleText: 'เพิ่มข้อมูลไม่สำเร็จ',
                                 });
@@ -194,5 +183,4 @@
             }
         });
     }
-
-</script>
+</script> 
