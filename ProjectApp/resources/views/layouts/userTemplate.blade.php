0c40439b84c2e8399656921b3fde193e313ae8a7
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-10-09 19:46:37 +0700

    koa

--- ProjectApp/resources/views/layouts/userTemplate.blade.php
+++ ProjectApp/resources/views/layouts/userTemplate.blade.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="stylesheet" href="{{ asset('node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
+
     @stack('css')
     <title>User Template</title>
 </head>
 <body>
     <div class="body">
-        @yield('login')
-
-
+@guest
+@yield('login')
+@endguest
         <nav>
-            
+
         </nav>
         <div class="content">
             @yield('content')
@@ -25,5 +28,6 @@
 
 @stack('js')
 
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 </body>
 </html>
--- ProjectApp/resources/views/login/index.blade.php
+++ ProjectApp/resources/views/login/index.blade.php
@@ -5,12 +5,47 @@
 @endpush
 
 @section('login')
-    <div class="body-login">
-        <div class="form-login">
-
+<div class="body-login">
+    <div class="form-login container">
+        <div class="head-login text-center">
+            <div class="logo-head">
+                <img class="p-0" src="{{url('images/logo.png')}}" alt="" width="150px" height="150px">
+            </div>
+            <p class="p-3">GE PETTITION</p>
         </div>
-        <div class="form-register">
-
+        <div class="body-login">
+            <form class="container" action="">
+                <h3>เข้าสู่ระบบ</h3>
+                <div class="form-group p-5">
+                    <label for="userId">รหัสนักศึกษา/รหัสอาจารย์</label>
+                    <input type="text" class="form-control" name="userId" id="userId" placeholder="">
+                    <br>
+                    <label for="pass">รหัสผ่าน</label>
+                    <input type="password" class="form-control" name="pass" id="pass" placeholder="">
+                </div>
+                <div class="container p-3 text-center">
+                    <h1>reCAPTCHA</h1>
+                </div>
+                <div class="text-center">
+                    <button class="btn-sub text-center" type="submit">เข้าสู่ระบบ</button>
+                </div>
+            </form>
         </div>
     </div>
+    <div class="form-register">
+        <div class="box-text container">
+            <dl class="row container p-5">
+                <dd class="col-md-12">
+                    <p class="m-0">PETTITION คืออะไร ?</p>
+                    <span>เว็บไซต์ส่งแบบคำร้องของมหาวิทยาลัยราชภัฐสวนสุนันทา</span>
+                    <hr>
+                    <span>คุณต้องการส่งแบบคำร้องแต่ยังไม่ได้เปิดใช้งานใช่หรือไม่ ?</span>
+                </dd>
+            </dl>
+            <div class="text-center m-0 p-0">
+                <a href="#" class="btn">สมัครเข้าใช้งาน</a>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
