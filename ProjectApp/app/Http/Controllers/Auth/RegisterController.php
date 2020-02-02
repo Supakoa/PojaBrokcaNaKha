e12a232e6e5e230c10daba21349224c50ff54f7a
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-02-02 16:38:36 +0700

    Update RegisterController.php

--- ProjectApp/app/Http/Controllers/Auth/RegisterController.php
+++ ProjectApp/app/Http/Controllers/Auth/RegisterController.php
@@ -26,11 +26,11 @@ class RegisterController extends Controller
     use RegistersUsers;
 
 
-    public function index(){
+    public function showRegistrationForm()
+    {
         $fac = fac::all();
         return view('login.element.reGister')->with('faculties',$fac);
     }
-
 
 
     /**
@@ -79,8 +79,5 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
-    public function showRegistrationForm()
-    {
-        return view('login.element.reGister');
-    }
+
 }
