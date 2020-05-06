d6082cb7cb2e3be7929643015afd6c1b803e1b45
Supakoa <41123305+Supakoa@users.noreply.github.com>
2020-05-06 21:54:40 +0700

    koa axios

--- laravel-react/resources/js/components/subdirect/contents/Home.js
+++ laravel-react/resources/js/components/subdirect/contents/Home.js
@@ -3,12 +3,14 @@ import MaterialTable from 'material-table';
 import StickyHaedTable from "./tableData/StickyHaedTable";
 import SortingAndSelecting from "./tableData/SortingAndSelecting";
 import axios from 'axios';
+import {Image} from "react-bootstrap";
 
 
 export default class Home extends Component{
     constructor(props) {
         super(props);
         this.state = {
+            persons: null,
             columns: [
                 { title: 'Name', field: 'name' },
                 { title: 'Surname', field: 'surname' },
@@ -22,73 +24,92 @@ export default class Home extends Component{
                 { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34,}
             ],
         }
-        this.persons = []
-
     }
 
     componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
+
+    }
+
+    getPersons = async () => {
+        await axios.get(`https://reqres.in/api/users?page=2`)
             .then(res => {
-                const persons = res.data;
-                this.setState(this.persons);
+                // this.setState({persons: res.data})
+                return res
             })
+    }
+
+    checkItem(){
+        if(this.state.persons === null){
+            return <p> empty </p>
+        }else{
+            console.log(this.state.persons.data)
+            return (
+                <ul>
+                    {this.state.persons.data.map( person =>
+                        <Image key={person.id} src={person.avatar}/>
+                        // <li key={person.id}>
+                        //
+                        //     {person.email}
+                        // </li>
+                    )}
+                </ul>
+            )
+
+        }
     }
 
     render(){
         return(
             <div>
-                <ul>
-                    { this.persons.map(person => <li>{person.name}</li>)}
-                </ul>
+                {this.checkItem()}
 
-
-                <MaterialTable
-                    stickyHeader
-                    title="Editable Example"
-                    columns={this.state.columns}
-                    data={this.state.data}
-                    editable={{
-                        onRowAdd: (newData) =>
-                            new Promise((resolve) => {
-                                setTimeout(() => {
-                                    resolve();
-                                    this.setState((prevState) => {
-                                        const data = [...prevState.data];
-                                        data.push(newData);
-                                        return { ...prevState, data };
-                                    });
-                                }, 600);
-                            }),
-                        onRowUpdate: (newData, oldData) =>
-                            new Promise((resolve) => {
-                                setTimeout(() => {
-                                    resolve();
-                                    if (oldData) {
-                                       this.setState((prevState) => {
-                                            const data = [...prevState.data];
-                                            data[data.indexOf(oldData)] = newData;
-                                            return { ...prevState, data };
-                                        });
-                                    }
-                                }, 600);
-                            }),
-                        onRowDelete: (oldData) =>
-                            new Promise((resolve) => {
-                                setTimeout(() => {
-                                    resolve();
-                                   this.setState((prevState) => {
-                                        const data = [...prevState.data];
-                                        data.splice(data.indexOf(oldData), 1);
-                                        return { ...prevState, data };
-                                    });
-                                }, 600);
-                            }),
-                    }}
-                />
-                <hr/>
-                <StickyHaedTable/>
-                <hr/>
-                <SortingAndSelecting/>
+                {/*<MaterialTable*/}
+                {/*    stickyHeader*/}
+                {/*    title="Editable Example"*/}
+                {/*    columns={this.state.columns}*/}
+                {/*    data={this.state.data}*/}
+                {/*    editable={{*/}
+                {/*        onRowAdd: (newData) =>*/}
+                {/*            new Promise((resolve) => {*/}
+                {/*                setTimeout(() => {*/}
+                {/*                    resolve();*/}
+                {/*                    this.setState((prevState) => {*/}
+                {/*                        const data = [...prevState.data];*/}
+                {/*                        data.push(newData);*/}
+                {/*                        return { ...prevState, data };*/}
+                {/*                    });*/}
+                {/*                }, 600);*/}
+                {/*            }),*/}
+                {/*        onRowUpdate: (newData, oldData) =>*/}
+                {/*            new Promise((resolve) => {*/}
+                {/*                setTimeout(() => {*/}
+                {/*                    resolve();*/}
+                {/*                    if (oldData) {*/}
+                {/*                       this.setState((prevState) => {*/}
+                {/*                            const data = [...prevState.data];*/}
+                {/*                            data[data.indexOf(oldData)] = newData;*/}
+                {/*                            return { ...prevState, data };*/}
+                {/*                        });*/}
+                {/*                    }*/}
+                {/*                }, 600);*/}
+                {/*            }),*/}
+                {/*        onRowDelete: (oldData) =>*/}
+                {/*            new Promise((resolve) => {*/}
+                {/*                setTimeout(() => {*/}
+                {/*                    resolve();*/}
+                {/*                   this.setState((prevState) => {*/}
+                {/*                        const data = [...prevState.data];*/}
+                {/*                        data.splice(data.indexOf(oldData), 1);*/}
+                {/*                        return { ...prevState, data };*/}
+                {/*                    });*/}
+                {/*                }, 600);*/}
+                {/*            }),*/}
+                {/*    }}*/}
+                {/*/>*/}
+                {/*<hr/>*/}
+                {/*<StickyHaedTable/>*/}
+                {/*<hr/>*/}
+                {/*<SortingAndSelecting/>*/}
             </div>
         );
     };
