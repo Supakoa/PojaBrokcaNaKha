ea475b6be0cb7420021e8e5fa90fa3ba84568bbd
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2020-05-10 20:50:55 +0700

    split module App and index.js

--- laravel-react/resources/js/components/App.js
+++ laravel-react/resources/js/components/App.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './subdirect/Main';
 import LogIn from "../log-in/LogIn";
-
 
 class App extends Component {
     constructor(props) {
@@ -47,6 +45,6 @@ class App extends Component {
 
 export default App;
 
-if (document.getElementById('root')) {
-    ReactDOM.render(<App />, document.getElementById('root'));
-}
+// if (document.getElementById('root')) {
+//     ReactDOM.render(<App />, document.getElementById('root'));
+// }
