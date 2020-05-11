024e70f888fbd0691031dab943be4c3722619f81
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2020-05-11 23:21:15 +0700

    full tutorial resux

--- laravel-react/resources/js/components/subdirect/contents/Temp.js
+++ laravel-react/resources/js/components/subdirect/contents/Temp.js
@@ -2,15 +2,19 @@
  * TODO: Can delete it
  */
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../../../redux/actions";
 
 function Temp () {
     const counter = useSelector(state => state.counter)
     const isLogged = useSelector(state => state.isLogged)
+    const dispatch = useDispatch()
 
     return (
         <div className="Temp">
             <h1>Counter: {counter}</h1>
+            <button className="p-2 m-2" onClick={() => dispatch(increment(1))}> + </button>
+            <button className="p-2 m-2" onClick={() => dispatch(decrement(2))}> - </button>
             {
                 isLogged ? <h1>welcome</h1> : <h1>login</h1>
             }
