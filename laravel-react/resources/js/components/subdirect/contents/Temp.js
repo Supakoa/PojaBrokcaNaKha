996faa200b4eec6ab53b7dd005d8f98bebd22791
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2020-05-11 18:02:53 +0700

    Update Temp.js

--- laravel-react/resources/js/components/subdirect/contents/Temp.js
+++ laravel-react/resources/js/components/subdirect/contents/Temp.js
@@ -1,12 +1,19 @@
+/**
+ * TODO: Can delete it
+ */
 import React from "react";
 import { useSelector } from "react-redux";
 
 function Temp () {
     const counter = useSelector(state => state.counter)
+    const isLogged = useSelector(state => state.isLogged)
 
     return (
         <div className="Temp">
             <h1>Counter: {counter}</h1>
+            {
+                isLogged ? <h1>welcome</h1> : <h1>login</h1>
+            }
         </div>
     )
 }
