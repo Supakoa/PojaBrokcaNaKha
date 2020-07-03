5fd2a2e0a9a00113f241bbdfba9b3108696d5f3f
Supakoa <41123305+Supakoa@users.noreply.github.com>
2020-07-04 01:44:41 +0700

    koa

---
+++ laravel-react/resources/js/components/student/context/Index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { table } from "./table";
+
+export const TeableReportCxt = React.createContext(table);
+export const ProfileContext = React.createContext({
+    user: null
+});
