b00868b74f544ddde58178d76287341c2b5e4612
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-07-11 04:51:42 +0700

    Register SwitchingLanguage

--- laravel-react/resources/js/components/auth/sign-up/FormRegister.js
+++ laravel-react/resources/js/components/auth/sign-up/FormRegister.js
@@ -1,12 +1,12 @@
 import React from "react";
 import LogoRegis from "./LogoReagis";
-import { Col, Form, Button, Spinner } from "react-bootstrap";
+import {Col, Form, Button, Spinner, Row} from "react-bootstrap";
 import SimpleReactValidator from 'simple-react-validator';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 export default function FormRegister() {
     // const dispatch = useDispatch();
-    const { t, i18n, ready } = useTranslation('', { useSuspense: false });
+    const {t, i18n, ready} = useTranslation('', {useSuspense: false});
     const [_select, setSelect] = React.useState(true);
     const [_majors, setMajors] = React.useState([]);
     const [_facs, setFacs] = React.useState([]);
@@ -18,7 +18,7 @@ export default function FormRegister() {
 
     const _fetchFaculties = async () => {
         await axios.get(`http://127.0.0.1:8000/api/faculties`, {}).then(res => {
-            const { success } = res.data;
+            const {success} = res.data;
             setFacs(success);
         });
         // .catch(error => {
@@ -44,32 +44,42 @@ export default function FormRegister() {
     }, []);
 
     const handleChanges = event => {
-        const { name, value } = event.target;
+        const {name, value} = event.target;
         serForms({
             ..._forms,
             [name]: value
         });
     };
 
-    const handleOnBlur = event => {};
+    const handleOnBlur = event => {
+    };
 
-    const handleOnClick = event => {};
+    const handleOnClick = event => {
+    };
     const handleIsInvalid = event => {
         // const {name,value} = event.target;
         //
         // return _validator.fieldValid(name);
     };
 
+    const switchingLanguage = () => {
+        if (i18n.language === "en") {
+            i18n.changeLanguage("th");
+        } else {
+            i18n.changeLanguage("en");
+        }
+    };
+
     return (
         <Form className="p-4 w-75 m-auto">
-            <LogoRegis />
+            <LogoRegis/>
             <Form.Row className="mt-4">
                 <Form.Group as={Col} controlId="title" sm={2} md={3} lg={3}>
-                    <Form.Label>{t('title')}</Form.Label>
+                    <Form.Label>{t('user.title')}</Form.Label>
                     <Form.Control
                         required
                         type="text"
-                        placeholder={t('title')}
+                        placeholder={t('user.title')}
                         name="title"
                         onChange={handleChanges}
                         onKeyUp={handleChanges}
@@ -83,22 +93,22 @@ export default function FormRegister() {
                     </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} controlId="firstName">
-                    <Form.Label>ชื่อ</Form.Label>
+                    <Form.Label>{t("user.firstName")}</Form.Label>
                     <Form.Control
                         required
                         type="text"
-                        placeholder="ชื่อ"
+                        placeholder={t("user.firstName")}
                         name="firstName"
                         onChange={handleChanges}
                     />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="lastName">
-                    <Form.Label>นามสกุล</Form.Label>
+                    <Form.Label>{t("user.lastName")}</Form.Label>
                     <Form.Control
                         required
                         type="text"
-                        placeholder="นามสกุล"
+                        placeholder={t("user.lastName")}
                         name="lastName"
                         onChange={handleChanges}
                     />
@@ -107,34 +117,34 @@ export default function FormRegister() {
 
             <Form.Row>
                 <Form.Group as={Col} md={8} controlId="studentId">
-                    <Form.Label>รหัสนักศึกษา</Form.Label>
+                    <Form.Label>{t("user.studentId")}</Form.Label>
                     <Form.Control
                         maxLength={11}
                         required
                         type="text"
-                        placeholder="รหัสนักศึกษา"
+                        placeholder={t("user.studentId")}
                         name="studentId"
                         onChange={handleChanges}
                     />
                 </Form.Group>
 
                 <Form.Group as={Col} md={6} controlId="password">
-                    <Form.Label>รหัสผ่าน</Form.Label>
+                    <Form.Label>{t("user.password")}</Form.Label>
                     <Form.Control
                         required
                         type="text"
-                        placeholder="รหัสผ่าน"
+                        placeholder={t("user.password")}
                         name="password"
                         onChange={handleChanges}
                     />
                 </Form.Group>
 
                 <Form.Group as={Col} md={6} controlId="conPassword">
-                    <Form.Label>ยืนยันรหัสผ่าน</Form.Label>
+                    <Form.Label>{t("user.confirmPassword")}</Form.Label>
                     <Form.Control
                         required
                         type="text"
-                        placeholder="ยืนยัน รหัสผ่าน"
+                        placeholder={t("user.confirmPassword")}
                         name="conPassword"
                         onChange={handleChanges}
                     />
@@ -143,7 +153,7 @@ export default function FormRegister() {
 
             <Form.Row>
                 <Form.Group as={Col} controlId="email">
-                    <Form.Label>อีเมล</Form.Label>
+                    <Form.Label>{t("user.email")}</Form.Label>
                     <Form.Control
                         required
                         type="email"
@@ -154,12 +164,12 @@ export default function FormRegister() {
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="phone">
-                    <Form.Label>เบอร์โทรศัพท์</Form.Label>
+                    <Form.Label>{t("user.phoneNumber")}</Form.Label>
                     <Form.Control
                         maxLength={10}
                         required
                         type="text"
-                        placeholder="เบอร์โทรศัพท์"
+                        placeholder={t("user.phoneNumber")}
                         name="phone"
                         onChange={handleChanges}
                     />
@@ -168,7 +178,7 @@ export default function FormRegister() {
 
             <Form.Row>
                 <Form.Group as={Col} controlId="faculty">
-                    <Form.Label>คณะ</Form.Label>
+                    <Form.Label>{t("user.faculty")}</Form.Label>
                     <Form.Control
                         required
                         as="select"
@@ -176,12 +186,12 @@ export default function FormRegister() {
                         name="faculty"
                         onChange={handleChanges}
                     >
-                        <option value="">คณะ</option>
+                        <option value="">{t("user.faculty")}</option>
                     </Form.Control>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="major">
-                    <Form.Label>สาขา</Form.Label>
+                    <Form.Label>{t("user.major")}</Form.Label>
                     <Form.Control
                         required
                         as="select"
@@ -190,14 +200,23 @@ export default function FormRegister() {
                         onChange={handleChanges}
                         disabled={_select}
                     >
-                        <option value="">สาขา</option>
+                        <option value="">{t("user.major")}</option>
                     </Form.Control>
                 </Form.Group>
             </Form.Row>
+            <Row>
+                <Col xl= {3}>
+                    <Button variant="primary" onClick={handleOnClick}>
+                        {_loading ? "ยืนยัน" : <Spinner animation="border"/>}
+                    </Button>
+                </Col>
+                <Col xl={6}>
 
-            <Button variant="primary" onClick={handleOnClick}>
-                {_loading ? "ยืนยัน" : <Spinner animation="border" />}
-            </Button>
+                </Col>
+                <Col xl= {3}>
+                    <Button type="button" onClick={switchingLanguage}> SwitchingLanguage </Button>
+                </Col>
+            </Row>
         </Form>
     );
 }
