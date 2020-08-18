238f6eeeb0d2eb22ab280dfa73ecf898fdc9bcee
Supakoa <41123305+Supakoa@users.noreply.github.com>
2020-08-19 00:03:14 +0700

    koa

--- laravel-react/resources/js/components/adminPage/navfolder/headerNav/index.js
+++ laravel-react/resources/js/components/adminPage/navfolder/headerNav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Spinner, Image } from "react-bootstrap";
+import { Navbar, Spinner, Image, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Logo from "./logo.png";
 import SignOutBtn from "../../../auth/sign-out";
@@ -8,8 +8,8 @@ import { useSelector } from "react-redux";
 export default function HeaderNav(props) {
     const _user = useSelector(s => s.userState);
     return (
-        <Navbar bg="light">
-            <Navbar.Brand href={props.path} className="text-info">
+        <Navbar bg="info">
+            <Navbar.Brand href={props.path} className="text-light">
                 <Image
                     src={Logo}
                     width="30"
@@ -20,29 +20,31 @@ export default function HeaderNav(props) {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="petition-nav" />
             <Navbar.Collapse id="petition-nav">
-                <div className="d-flex align-items-center justify-content-around text-light">
-                    {Object.keys(_user).length !== 0 ? (
-                        <Link
-                            to="#"
-                            className="text-light d-flex align-items-center justify-content-center px-2"
-                        >
-                            <Image
-                                src="https://img.icons8.com/plasticine/2x/user.png"
-                                className="d-inline-block align-top"
-                                width="30"
-                                height="30"
-                                rounded="true"
-                            />{" "}
-                            {`${_user.title} ${_user.first_name} ${_user.last_name}`}
+                <Nav className="mr-auto ">
+                    <div className="d-flex align-items-center justify-content-end text-light text-right">
+                        {Object.keys(_user).length !== 0 ? (
+                            <Link
+                                to="#"
+                                className="text-light d-flex align-items-center justify-content-center px-2"
+                            >
+                                <Image
+                                    src="https://img.icons8.com/plasticine/2x/user.png"
+                                    className="d-inline-block align-top"
+                                    width="30"
+                                    height="30"
+                                    rounded="true"
+                                />{" "}
+                                {`${_user.title} ${_user.first_name} ${_user.last_name}`}
+                            </Link>
+                        ) : (
+                            <Spinner animation="border" size="sm" />
+                        )}
+                        <Link to="#" className="px-3">
+                            <SwitchingLanguageBtn />
                         </Link>
-                    ) : (
-                        <Spinner animation="border" size="sm" />
-                    )}
-                    <Link to="#" className="px-3">
-                        <SwitchingLanguageBtn />
-                    </Link>
-                    <SignOutBtn className="text-light" />
-                </div>
+                        <SignOutBtn className="text-light" />
+                    </div>
+                </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
