f0ed1046a4754c99fd07a996307d2c919a476e00
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-08-30 18:58:16 +0700

    Form

--- laravel-react/app/Http/Controllers/FormsController.php
+++ laravel-react/app/Http/Controllers/FormsController.php
@@ -14,14 +14,17 @@ class FormsController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return Form[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index(Request $request)
     {
         $forms = Form::all();
         if (auth()->user()->role_id == 1)
             foreach ($forms as $form) {
-                $form->groups;
+                if ($form->all_state == 0)
+                    $form->groups = [];
+                for ($i = 0; $i < $form->all_state; $i++)
+                    $form->groups[$i] = $form->groups()->wherePivot('state', $i + 1)->get();;
             }
 
         return $forms;
@@ -34,7 +37,8 @@ class FormsController extends Controller
      *
      * @return Response
      */
-    public function store(Request $request)
+    public
+    function store(Request $request)
     {
 
         $form = Form::create($request->all());
@@ -49,7 +53,8 @@ class FormsController extends Controller
      *
      * @return Response
      */
-    public function show($id)
+    public
+    function show($id)
     {
         $form = Form::findOrFail($id);
 
@@ -64,7 +69,8 @@ class FormsController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, Form $form)
+    public
+    function update(Request $request, Form $form)
     {
 
         $form->update($request->all());
@@ -77,29 +83,34 @@ class FormsController extends Controller
      * @param Form $form
      * @return Response
      */
-    public function destroy(Form $form)
+    public
+    function destroy(Form $form)
     {
         $form->groups()->detach();
         Form::destroy($form->id);
         return response()->json(null, 204);
     }
 
-    public function documents(Form $form)
+    public
+    function documents(Form $form)
     {
         return response()->json(['success' => $form->documents], $this->successStatus);
     }
 
-    public function groups(Form $form)
+    public
+    function groups(Form $form)
     {
         return response()->json(['success' => $form->groups], $this->successStatus);
     }
 
-    public function groupsByState(Form $form, int $state)
+    public
+    function groupsByState(Form $form, int $state)
     {
         return response()->json(['success' => $form->groups()->wherePivot("state", $state)->get()], $this->successStatus);
     }
 
-    public function addGroup(Form $form, Request $request)
+    public
+    function addGroup(Form $form, Request $request)
     {
         $groups = $form->groups()->where("group_id", $request->input("group_id"));
         if ($groups->count() > 0)
@@ -108,7 +119,8 @@ class FormsController extends Controller
         return $form->groups;
     }
 
-    public function deleteGroup(Form $form, Request $request)
+    public
+    function deleteGroup(Form $form, Request $request)
     {
         $form->groups()->detach($request->input("group_id"));
         return $form->groups;
