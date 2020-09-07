e08a9f8eff3223a9f76e04fed77d1f3d51fd3f84
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-09-07 22:49:29 +0700

    Revert "Test Chat"
    This reverts commit 2c0f3ecd461cdc9978915f4aabed7f7b0bb83dbb.

--- laravel-react/app/Http/Controllers/API/UserController.php
+++ laravel-react/app/Http/Controllers/API/UserController.php
@@ -191,18 +191,14 @@ class UserController extends Controller
             });
             foreach ($users as $user) {
                 $count_unread =  Message::query()->where("user_id", $user->id)->whereNull("admin_id")->where("read",0)->count();
-                $user["count_unread"] = $count_unread;
 //                event(new ChatEchoToAdmin($request->get("message"), $user_id, $count_messages, $count_unread));
-                $count_messages =  Message::query()->where("user_id", $user->id)->count();
-                $user["count_messages"] = $count_messages;
+                $user["count_unread"] = $count_unread;
             }
             return response()->json(['success' => array_values($users->all())], $this->successStatus);
         } else {
-                $count_unread =  Message::query()->where("user_id", $user->id)->whereNotNull("admin_id")->where("read",0)->count();
+                $count_unread =  Message::query()->where("user_id", $user->id)->whereNull("admin_id")->where("read",0)->count();
 //                event(new ChatEchoToAdmin($request->get("message"), $user_id, $count_messages, $count_unread));
-            $count_messages =  Message::query()->where("user_id", $user->id)->count();
-
-            return response()->json(['success' => $user->messages,'count_unread'=>$count_unread,"count_messages"=>$count_messages], $this->successStatus);
+            return response()->json(['success' => $user->messages,'count_unread',$count_unread], $this->successStatus);
         }
     }
 
