7ef46e1457b6238e7c94b0636eb596047d6f7deb
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-09-07 22:55:37 +0700

    Revert "Revert "Test Chat""
    This reverts commit e08a9f8eff3223a9f76e04fed77d1f3d51fd3f84.

--- laravel-react/resources/js/components/adminPage/message/TemplateMessage.js
+++ laravel-react/resources/js/components/adminPage/message/TemplateMessage.js
@@ -11,15 +11,15 @@ import {
 } from "react-bootstrap";
 import BoxMessage from "./BoxMessage";
 import FormSend from "./FormSend";
-import { useTranslation } from "react-i18next";
-import { getMessages } from "../../middleware/axios/getMessages";
+import {useTranslation} from "react-i18next";
+import {getMessages} from "../../middleware/axios/getMessages";
 import ListMessages from "../../student/components/message/list-message";
 import axios from "axios";
-import { _urlGetMessages } from "../../middleware/apis";
+import {_urlGetMessages} from "../../middleware/apis";
 import headerConfig from "../../middleware/headerConfig";
 import Swal from "sweetalert2";
-import { forEach } from "react-bootstrap/cjs/ElementChildren";
-import { _URL } from "../../middleware/URL";
+import {forEach} from "react-bootstrap/cjs/ElementChildren";
+import {_URL} from "../../middleware/URL";
 import LoadingComponent from "../../LoadingComponent/Loading";
 
 export default function TemplateMessage() {
@@ -27,22 +27,22 @@ export default function TemplateMessage() {
     const [isopen, setIsOpen] = React.useState(true);
     const [userIdOpen, setUserIdOpen] = React.useState(0);
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     const fetchMessages = async token => {
         const _getAll = await getMessages(token);
         if (_getAll) setListUsers(_getAll);
     };
 
-    const read = async id =>{
+    const read = async id => {
         let user = _listUsers.find(value => {
             // console.log("value", value.id);
             return value.id == id;
         });
-        console.log("userIdOpen ",userIdOpen);
+        console.log("userIdOpen ", userIdOpen);
         console.log("e.target.name ", id);
 
-        if (!!user && user.count_unread > 0 && (userIdOpen==0 ? true : userIdOpen == id) ) {
+        if (!!user && user.count_unread > 0 && (userIdOpen == 0 ? true : userIdOpen == id)) {
             axios
                 .get(`${_URL}/api/messages/${id}/read`, {
                     headers: {
@@ -60,32 +60,41 @@ export default function TemplateMessage() {
         }
     };
     let channel = window.Echo.channel("channel-chat");
-    channel.listen(".event-chat-admin", async function(data) {
-
+    channel.listen(".event-chat-admin", async function (data) {
+        console.log(data)
         let user = _listUsers.find(value => value.id === data.user_id);
         setIsOpen(true);
         if (!!user) {
-
-            user["count_unread"] = data.count_messages_unread;
-            let tmp_message = {};
-            tmp_message.id = data.count_messages + 1;
-            tmp_message.message = data.message;
-            tmp_message.admin_id = null;
-            tmp_message.user_id = data.user_id;
-            if (
-                user.messages.findIndex(
-                    value => value.id == tmp_message.id
-                ) === -1
-            ) {
-                console.log("Data : ",data);
-                user.messages.push(tmp_message);
-                setListUsers([..._listUsers]);
+            if (data.count_messages != (Number(user.count_messages) + 1)) {
+                console.log("data.count_messages : ", data.count_messages)
+                console.log("user.count_messages : ", Number(user.count_messages) + 1)
+                getMessages(localStorage._authLocal).then(value => {
+                    setListUsers(value)
+                    // setAPI(false);
+                })
+            } else {
+                user["count_unread"] = data.count_messages_unread;
+                let tmp_message = {};
+                tmp_message.id = data.count_messages + 1;
+                tmp_message.message = data.message;
+                tmp_message.admin_id = null;
+                tmp_message.user_id = data.user_id;
+                if (
+                    user.messages.findIndex(
+                        value => value.id == tmp_message.id
+                    ) === -1
+                ) {
+                    console.log("Data : ", data);
+                    user.messages.push(tmp_message);
+                    setListUsers([..._listUsers]);
+                }
             }
+
         } else if (data.count_messages == 1) {
             const _getAll = await getMessages(localStorage._authLocal);
             if (_getAll) setListUsers(_getAll);
         }
-        if (userIdOpen == data.user_id){
+        if (userIdOpen == data.user_id) {
             read(data.user_id);
         }
         var element = document.getElementById("chatBody");
@@ -102,18 +111,18 @@ export default function TemplateMessage() {
                 element.scrollTop = element.scrollHeight;
             }, 200);
         }
-            setUserIdOpen(id) ;
-            setTimeout(function () {
-                console.log(2000)
-                read(id)
-            },2000)
+        setUserIdOpen(id);
+        setTimeout(function () {
+            console.log(2000)
+            read(id)
+        }, 2000)
 
 
     };
 
     React.useEffect(() => {
         const abort = new AbortController();
-        fetchMessages(localStorage._authLocal, { signal: abort.signal });
+        fetchMessages(localStorage._authLocal, {signal: abort.signal});
 
         return () => {
             abort.abort();
@@ -122,7 +131,7 @@ export default function TemplateMessage() {
 
     React.useEffect(() => {
 
-        console.log("_listUsers : ",_listUsers)
+        console.log("_listUsers : ", _listUsers)
     }, [_listUsers]);
 
     return (
@@ -130,7 +139,7 @@ export default function TemplateMessage() {
             <Row>
                 <Col xs={12} sm={12} md={4} lg={4} className="border-right">
                     <p>{t("sender")}</p>
-                    <hr />
+                    <hr/>
                     <ListGroup variant="flush" className="rounded">
                         {_listUsers.length > 0 ? (
                             _listUsers.map((item, idx) => {
@@ -159,13 +168,13 @@ export default function TemplateMessage() {
                                 );
                             })
                         ) : (
-                            <LoadingComponent />
+                            <LoadingComponent/>
                         )}
                     </ListGroup>
                 </Col>
                 <Col xs={12} sm={12} md={8} lg={8}>
                     <p>{t("menu.message")}</p>
-                    <hr />
+                    <hr/>
                     <Tab.Content>
                         {_listUsers.map((userMessages, idx) => {
                             return (
@@ -205,7 +214,7 @@ export default function TemplateMessage() {
                                                 userId={userMessages.id}
                                                 _listUsers={_listUsers}
                                                 setListUsers={setListUsers}
-                                                read = {read}
+                                                read={read}
                                             />
                                         </Card.Footer>
                                     </Card>
