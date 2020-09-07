5e972cc0bb079277fe55d9c896f3d5bb8fc3aa6b
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2020-09-07 23:06:51 +0700

    Update UserController.php

--- laravel-react/app/Http/Controllers/API/UserController.php
+++ laravel-react/app/Http/Controllers/API/UserController.php
@@ -66,6 +66,9 @@ class UserController extends Controller
         $user = User::create($input);
         $success['token'] = $user->createToken('MyApp')->accessToken;
         $success['name'] = $user->name;
+        if ($user->major!=null){
+            $user->major->faculty;
+        }
         $success['user'] = $user;
 
         return response()->json(['success' => $success], $this->successStatus);
